05b08aa4633ee7b01dde9f269db459e37b814eb1
Diwakar Jaiswal <38224086+diwakarjaiswal880@users.noreply.github.com>
2020-12-07 22:06:15 +0530

    Update rest_api_in_go.go

--- rest_api_in_go.go
+++ rest_api_in_go.go
@@ -107,9 +107,3 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
 
-// Request sample
-// {
-// 	"isbn":"4545454",
-// 	"title":"Book Three",
-// 	"author":{"firstname":"Harry","lastname":"White"}
-// }
