0ab74a418b7c081e5006b64ebd6fd864da60003e
Diwakar Jaiswal <38224086+diwakarjaiswal880@users.noreply.github.com>
2020-12-10 10:25:17 +0530

    Update mysql.go

--- mysql.go
+++ mysql.go
@@ -11,8 +11,9 @@ func main() {
 	fmt.Println("Go MySQL Tutorial")
 
 	// Open up our database connection.
+	// I've set up a database on my local machine using phpmyadmin.
 	// The database is called testDb
-	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/testdb")
+	db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/testdb")
 
 	// if there is an error opening the connection, handle it
 	if err != nil {
@@ -23,14 +24,15 @@ func main() {
 	// executing
 	defer db.Close()
 
-	// perform a db.Query insert
-    insert, err := db.Query("INSERT INTO testdb VALUES ( 2, 'TEST' )")
-
-    // if there is an error inserting, handle it
-    if err != nil {
-        panic(err.Error())
-    }
-    // be careful deferring Queries if you are using transactions
-    defer insert.Close()
+	//perform a db.Query insert
+	insert, err := db.Query("INSERT INTO student VALUES ( 1, 'Rohan' )")
+	insert, err = db.Query("INSERT INTO student VALUES ( 2, 'Sohan' )")
+	insert, err = db.Query("INSERT INTO student VALUES ( 3, 'Mohan' )")
+	// if there is an error inserting, handle it
+	if err != nil {
+		panic(err.Error())
+	}
+	// be careful deferring Queries if you are using transactions
+	defer insert.Close()
 
 }
